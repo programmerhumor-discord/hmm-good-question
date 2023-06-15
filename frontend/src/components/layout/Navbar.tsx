c43a957df9564b89f0026bc24fbbcf87fb05f7d5
elijah629 <62805599+elijah629@users.noreply.github.com>
2023-06-14 20:10:44 -0700

    fix: remove old file

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -4,17 +4,6 @@ import Link from "next/link";
 export function Navbar() {
 	return (
 		<nav className="navbar rounded-box justify-between bg-base-100">
-			{/* <div className="navbar-start hidden sm:flex">
-				<Link
-					href="/"
-					className="btn-ghost btn text-xl normal-case">
-					Hmm. Good Question
-				</Link>
-			</div>
-			<div className="navbar-end join">
-				<Link href="/login" className="btn-accent join-item btn">Login</Link>
-				<Link href="/signup" className="join-item btn">Signup</Link>
-			</div> */}
 			<div>
 				<Link
 					href="/"
