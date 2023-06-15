948808d3ca67c8396161f4a7a7d209a7e9c6294c
ateebplayz <challengebudies9@gmail.com>
2023-06-15 06:00:41 +0500

    Addition: Navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,8 +1,35 @@
-import "../app/globals.css";
+import Link from "next/link"
+import { BiHome, BiSearchAlt2 } from "react-icons/bi"
+import { BsPersonFill } from 'react-icons/bs'
 export default function NavBar() {
 	return (
 		<div className="navbar-container">
-			<h1 className="navbar-name">Hmm. Good Question</h1>
+			<div className="navbar-left">
+				<Link className="navbar-linker" href='/'>
+					<p className="navbar-name">Hmm. Good Question</p>
+				</Link>
+				<Link className="navbar-linker-home" href='/'>
+					<p className="navbar-home-text">Home</p>
+				</Link>
+				<Link className="navbar-linker-home-mobile" href='/'>
+					<BiHome color="#fff" size={20}/>
+				</Link>
+				<Link className="navbar-linker-home-mobile" style={{marginLeft: '10px'}} href='/'>
+					<BiSearchAlt2 color="#fff" size={20}/>
+				</Link>
+				<input className="navbar-search" placeholder="Search in Hmm. Good Question."/>
+			</div>
+			<div className="navbar-right">
+				<Link className="navbar-linker-login" href='/'>
+					<p className="navbar-home-text">Login</p>
+				</Link>
+				<Link className="navbar-linker-signup" href='/'>
+					<p className="navbar-home-text">Signup</p>
+				</Link>
+				<Link className="navbar-linker-home-mobile" style={{marginLeft: '10px'}} href='/'>
+					<BsPersonFill color="#fff" size={20}/>
+				</Link>
+			</div>
 		</div>
 	);
 }
